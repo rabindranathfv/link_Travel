2c6ae1425a12c99cc44690cd152dda2a423d4164
rabindranathfv <ferreiralox@gmail.com>
2017-11-29 21:47:23 -0300

    separando los template y dejando configuracion para envio sms

--- main_app/views.py
+++ main_app/views.py
@@ -2,7 +2,14 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.conf import settings
+
+#Mail library
+from django.core.mail import send_mail
 
 # Create your views here.
 def index(request):
 	return render(request, 'base.html')
+
+def contactanos(request):
+	return render(request,'base.html')
