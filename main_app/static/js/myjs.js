e84d6b5c1a89b9fb77a185fc3911d510310578ce
Osvani <osvacolijim@gmail.com>
2018-02-24 14:50:58 -0400

    Corregido bug donde no aparecia sidebar para versión movil

--- main_app/static/js/myjs.js
+++ main_app/static/js/myjs.js
@@ -1,9 +1,3 @@
-//Menu
-function openMenu(evt, menuName) {
-  var i, x, tablinks; x = document.getElementsByClassName("menu"); for (i = 0; i < x.length; i++) { x[i].style.display="none" ; } tablinks=d ocument.getElementsByClassName( "tablink"); for (i=0 ; i < x.length; i++) { tablinks[i].className=t ablinks[i].className.replace(
-   " w3-red", ""); } document.getElementById(menuName).style.display="block" ; evt.currentTarget.firstElementChild.className +=" w3-red" ; } document.getElementById( "myLink").click();
-
-
 // Modal Image Gallery
 function onClick(element) {
   document.getElementById("img01").src = element.src;
